785f8afb90dc7c1830da88439147906d6cf474d3
EC2 Default User <ec2-user@ip-172-16-15-145.ec2.internal>
2023-01-27 05:56:50 +0000

    initial commit

--- elasticsearch_setup.sh
+++ elasticsearch_setup.sh
@@ -1,6 +1,10 @@
-sudo yum install docker
-(sudo) docker pull docker.elastic.co/elasticsearch/elasticsearch:7.9.2
-sudo dockerd
+# set up docker
+sudo yum install docker -y
+sudo dockerd &
+
+# puller docker image for elasticsearch and run it
+sudo docker pull docker.elastic.co/elasticsearch/elasticsearch:7.9.2
 sudo docker run -d -p 9200:9200 -e "discovery.type=single-node" elasticsearch:7.9.2
-netstat -tulpn
-curl -X GET 'http://localhost:9200/_cat/indices?v'
+
+# test if elasticsearch is running
+curl localhost:9200
